a292a8d265383bed4324dde14aeaef63eb29fa4f
Justin Ochoa <just.och92@gmail.com>
2021-12-24 13:09:47 -0500

    cookie popup with animation by gsap

---
+++ index.js
@@ -0,0 +1,20 @@
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
+
+tl.fromTo(".cookie-container", { scale: 0 }, { scale: 1 });
+tl.fromTo(
+  ".cookie",
+  { opacity: 0, x: -50, rotation: "-45deg" },
+  { opacity: 1, x: 0, rotation: "0deg" }
+);
+tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  gsap.to(".cookie-container", {
+    opacity: 0,
+    y: 100,
+    duration: 0.75,
+    ease: "power1.out",
+  });
+});
